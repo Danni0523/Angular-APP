5d91767ba08324d620c6f975cd6d3249524b40cd
Har0l <65699208+H4r0l@users.noreply.github.com>
2023-10-23 18:55:58 -0500

    final

--- src/app/consultar-costos/consultar-costos.component.ts
+++ src/app/consultar-costos/consultar-costos.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2,  ElementRef} from '@angular/core';
+import data from '../../assets/data.json';
 
 @Component({
   selector: 'app-consultar-costos',
   templateUrl: './consultar-costos.component.html',
-  styleUrls: ['./consultar-costos.component.css']
+  styleUrls: ['./consultar-costos.component.css'],
 })
 export class ConsultarCostosComponent {
+  phones = data.phones;
+  selectedPhone: any;
 
+  constructor(private renderer: Renderer2, private el: ElementRef) {}
+
+  showDetail(phone: any, index: number) {
+    this.selectedPhone = phone;
+
+    // Enfocar el elemento de detalles
+    setTimeout(() => {
+      const detailElement = this.el.nativeElement.querySelector(
+        `#phone-detail-${index}`
+      );
+      if (detailElement) {
+        this.renderer
+          .selectRootElement(detailElement)
+          .scrollIntoView({ behavior: 'smooth' });
+      }
+    });
+  }
 }
--- src/app/costo-plan/costo-plan.component.ts
+++ src/app/costo-plan/costo-plan.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+// costo-plan.component.ts
+
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-costo-plan',
   templateUrl: './costo-plan.component.html',
-  styleUrls: ['./costo-plan.component.css']
+  styleUrls: ['./costo-plan.component.css'],
 })
-export class CostoPlanComponent {
+export class CostoPlanComponent implements OnInit {
+  monto: number = 0;
+  cuotas: number = 0;
+  pagoMensual: number = 0; // Inicializado a 0
 
+  ngOnInit() {
+    this.monto = 0;
+    this.cuotas = 0;
+  }
+
+  scrollToCalculator() {
+    const calculatorElement = document.getElementById('calculator');
+    if (calculatorElement) {
+      calculatorElement.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+
+  calcularPago() {
+    if (this.monto && this.cuotas) {
+      this.pagoMensual = this.monto / this.cuotas;
+    }
+  }
 }
